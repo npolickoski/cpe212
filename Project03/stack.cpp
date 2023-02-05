17f1a0875e2e1bd1ee6c442c9ba097ee3b75700e
Will Sciaroni <wsciaroni@protonmail.com>
2023-02-05 16:59:22 -0600

    Add initial implementation of Project03

--- Project03/stack.cpp
+++ Project03/stack.cpp
@@ -2,64 +2,114 @@
 
 void Stack::Resize(int n)
 {
+    try {
+        int* newArrayPtr = new int[2*num];
+        array = newArrayPtr;
+        num *= 2;
+    } catch(...) {
+        throw StackFull();
+    }
+
+    Push(n);
 }
 
-Stack::Stack(int n)
+Stack::Stack(int n) : array(nullptr), num(0), top(-1)
 {
+    Resize(n);
 }
 
 Stack::~Stack()
 {
+    try {
+        if (nullptr != array) {
+            delete array;
+        }
+    } catch(...) {}
 }
 
 void Stack::Push(int n)
 {
+    // In the unlikely case that you instantiate the class with 0 for n
+    if (Capacity() == 0 || nullptr == array) throw StackFull();
+
+    if (IsFull()) {
+        Resize(n);
+    }
+    else {
+        top++;
+        array[top] = n;
+    }
 }
 
 void Stack::Pop()
 {
+    if (IsEmpty()) throw StackEmpty();
+    top--;
 }
 
 bool Stack::IsEmpty() const
 {
-    return false;
+    return (nullptr == array) || (top < 0) || (0 == Capacity());
 }
 
 bool Stack::IsFull() const
 {
-    return false;
+    return top >= Capacity() - 1;
 }
 
 void Stack::MakeEmpty()
 {
+    top = -1;
 }
 
 int Stack::Top() const
 {
-    return 0;
+    if (IsEmpty()) throw StackEmpty();
+
+    return array[top];
 }
 
 int Stack::Size() const
 {
-    return 0;
+    return top+1;
 }
 
 int Stack::Max() const
 {
-    return 0;
+    if (IsEmpty()) throw StackEmpty();
+
+    int maxSeen = INT_MIN;
+    for (int i = 0; i <= top && i <= Capacity(); i++) {
+        try {
+            maxSeen = std::max(maxSeen, Peek(i));
+        }
+        catch (StackInvalidPeek&) {}
+    }
+    return maxSeen;
 }
 
 int Stack::Min() const
 {
-    return 0;
+    if (IsEmpty()) throw StackEmpty();
+
+    int minSeen = INT_MIN;
+    for (int i = 0; i <= top; i++) {
+        try {
+            minSeen = std::min(minSeen, Peek(i));
+        }
+        catch (StackInvalidPeek&) {}
+    }
+    return minSeen;
 }
 
 int Stack::Peek(unsigned int n) const
 {
-    return 0;
+    // Protect against stack overflow verifying greater than 0
+    if (top < 0 || n > top || n < 0) throw StackInvalidPeek();
+    return array[top-n];
 }
 
 int Stack::Capacity() const
 {
-    return 0;
+    return num;
 }
