59ebf2e009e29904de2d54d1c407fb92464fafa9
Nick <npolickoski@gmail.com>
2023-02-05 12:47:23 -0600

    Add function stubs for stack class

---
+++ Project03/stack.cpp
@@ -0,0 +1,65 @@
+#include "stack.h"
+
+void Stack::Resize(int n)
+{
+}
+
+Stack::Stack(int n)
+{
+}
+
+Stack::~Stack()
+{
+}
+
+void Stack::Push(int n)
+{
+}
+
+void Stack::Pop()
+{
+}
+
+bool Stack::IsEmpty() const
+{
+    return false;
+}
+
+bool Stack::IsFull() const
+{
+    return false;
+}
+
+void Stack::MakeEmpty()
+{
+}
+
+int Stack::Top() const
+{
+    return 0;
+}
+
+int Stack::Size() const
+{
+    return 0;
+}
+
+int Stack::Max() const
+{
+    return 0;
+}
+
+int Stack::Min() const
+{
+    return 0;
+}
+
+int Stack::Peek(unsigned int n) const
+{
+    return 0;
+}
+
+int Stack::Capacity() const
+{
+    return 0;
+}
