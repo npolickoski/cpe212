c7a930dcbd84be355c9ea5ac26b7256f8924e003
Nick <npolickoski@gmail.com>
2023-02-26 15:53:59 -0600

    stack.cpp ver.2.5 - changes to INT_MAX and nullptr

--- Project03/stack.cpp
+++ Project03/stack.cpp
@@ -80,7 +80,7 @@ int Stack::Size() const
 
 int Stack::Max() const
 {
-    int MaxOfStack = I-2147483648;                      // stdlib value for minimum integr (header file not allowed to be used)
+    int MaxOfStack = -2147483648;                      // stdlib value for minimum integr (header file not allowed to be used)
 
     for (int i = 0; i < Size() && i < Capacity(); i++)  // for all values of the stack array that don't excede # elements and # of spaces on stack that can be occupied
     {
