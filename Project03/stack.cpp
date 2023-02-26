7e7bd9c7737bb032670b4152f1fd6f258b357a6e
Nick <npolickoski@gmail.com>
2023-02-26 16:48:42 -0600

    stack.cpp ver. #3 - added exception handeling

--- Project03/stack.cpp
+++ Project03/stack.cpp
@@ -5,22 +5,29 @@
 
 void Stack::Resize(int n)
 {
-    size_t new_size = 2*num;                            // creates variable for doubling stack size
-
-    int* newArrayPtr = new int[new_size];               // heap memory allocation for new array
-    for(size_t i = 0; i < top; i++) 
+    try
     {
-        newArrayPtr[i] = array[i];
+        size_t new_size = 2*num;                                // creates variable for doubling stack size
+
+        int* newArrayPtr = new int[new_size];                   // heap memory allocation for new array
+        for(size_t i = 0; i < top; i++) 
+        {
+            newArrayPtr[i] = array[i];
+        }
+
+        array = newArrayPtr;                                    // relocates new array back to original array but with size change
+        num = new_size;                                         // updates max size of the array to the doubled size
+    }
+    catch(...)
+    {
+        throw StackFull();
     }
 
-    array = newArrayPtr;                                // relocates new array back to original array but with size change
-    num = new_size;                                     // changes max size of the array to the doubled size
-
-    Push(n);                                            // adds next element if size of stack was exceded prior to resize
+    Push(n);                                                    // adds next element if size of stack was exceded prior to resize
 }
 
 
-Stack::Stack(int n)
+Stack::Stack(int n) : num(n), top(-1)
 {
     array = new int[num];
 }
@@ -28,63 +35,93 @@ Stack::Stack(int n)
 
 Stack::~Stack()                
 {
-    if (0 != array) 
+    try
     {
-        delete array;
+        if (0 != array) 
+        {
+            delete array;                                       // deallocates heap memory locations
+        }
     }
+    catch(...) {}
 }
 
 
 void Stack::Push(int n)
 {
-    top++;                                              // increases size of stack to accomodate more values
-    array[top] = n;                                     // adds new point of data onto the stack          
+    if (Capacity() == 0 || 0 == array)
+    {
+        throw StackFull();                                      // conditional for if there's nothing in the stack
+    } 
+
+    if (IsFull()) 
+    {
+        Resize(n);
+    }
+    else
+    {
+        top++;                                                  // increases size of stack to accomodate more values
+        array[top] = n;                                         // adds new point of data onto the stack          
+    }
 }
 
 
 void Stack::Pop()
 {
-    top--;                                              // decreases size of stack and thus removes top most element
+    if (IsEmpty()) 
+    {
+        throw StackEmpty();                                     // conditional for if stack is empty
+    }
+
+    top--;                                                      // decreases size of stack and thus removes top most element
 }
 
 
 bool Stack::IsEmpty() const
 {
-    return (Size() <= 0) || (0 == Capacity());;         // conditional if the top-most element of the stack is also the  bottem-most element
+    return (0 == array || Size() <= 0) || (0 == Capacity());    // conditional if the top-most element of the stack is also the  bottem-most element
 }
 
 
 bool Stack::IsFull() const
 {
-    return Size() >= Capacity();                        // conditional if the top-most element of the stack is in the highest index possible of the stack 
+    return Size() >= Capacity();                                // conditional if the top-most element of the stack is in the highest index possible of the stack 
 }
 
 
 void Stack::MakeEmpty()
 {
-    top = -1;                                           // removes all elements of the stack
+    top = -1;                                                   // removes all elements of the stack
 }
 
 
 int Stack::Top() const
 {
-    return array[top];                                  // returns top-most value of the stack w/o modifying said value
+    if (IsEmpty())
+    {
+        throw StackEmpty();                                     // conditional for if nothing is in the stack at all
+    }
+
+    return array[top];                                          // returns top-most value of the stack w/o modifying said value
 }
 
 
 int Stack::Size() const
 {
-    return (top + 1);                                   // returns # of elements in the stack 
+    return (top + 1);                                           // returns # of elements in the stack 
 }
 
 
 int Stack::Max() const
 {
-    int MaxOfStack = -2147483648;                      // stdlib value for minimum integr (header file not allowed to be used)
-
-    for (int i = 0; i < Size() && i < Capacity(); i++)  // for all values of the stack array that don't excede # elements and # of spaces on stack that can be occupied
+    if (IsEmpty())
     {
-        MaxOfStack = std::max(MaxOfStack, Peek(i));     // using stdlib function for maxiumum value between current min. value and current element in the stack
+        throw StackEmpty();                                     // conditional for if nothing is in the stack at all
+    }
+
+    int MaxOfStack = -2147483648;                               // stdlib value for minimum integr (header file not allowed to be used)
+    for (int i = 0; i < Size() && i < Capacity(); i++)          // for all values of the stack array that don't excede # elements and # of spaces on stack that can be occupied
+    {
+        MaxOfStack = std::max(MaxOfStack, Peek(i));             // using stdlib function for maxiumum value between current min. value and current element in the stack
     }
 
     return MaxOfStack;
@@ -93,11 +130,15 @@ int Stack::Max() const
 
 int Stack::Min() const
 {
-    int MinOfStack = -2147483648;                       // stdlib value for minimum integr (header file not allowed to be used)
-
-    for (int i = 0; i < Size() && i < Capacity(); i++)  // for all values of the stack array that don't excede # elements and # of spaces on stack that can be occupied
+    if (IsEmpty())
     {
-        MinOfStack = std::min(MinOfStack, Peek(i));     // using stdlib function for miniumum value between current min. value and current element in the stack
+        throw StackEmpty();                                     // conditional for if nothing is in the stack at all
+    }
+
+    int MinOfStack = -2147483648;                               // stdlib value for minimum integr (header file not allowed to be used)
+    for (int i = 0; i < Size() && i < Capacity(); i++)          // for all values of the stack array that don't excede # elements and # of spaces on stack that can be occupied
+    {
+        MinOfStack = std::min(MinOfStack, Peek(i));             // using stdlib function for miniumum value between current min. value and current element in the stack
     }
 
     return MinOfStack;
@@ -106,6 +147,15 @@ int Stack::Min() const
 
 int Stack::Peek(unsigned int n) const
 {
+    if (
+        Size() <= 0                                             // The stack is empty, no peek is valid
+        || n >= Size()                                          // The query is beyond the number of elements
+        || n < 0                                                // Negative is invalid for an unsigned
+    ) 
+    {
+        throw StackInvalidPeek();                                
+    }
+
     return array[top - n];
 }
 
