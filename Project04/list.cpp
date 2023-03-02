9de01c9ce28ebbe635b2cc77df280b220fa47477
Nick <npolickoski@gmail.com>
2023-03-01 18:31:38 -0600

    list.cpp v.0 - List ADT skeleton

---
+++ Project04/list.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <new>
+#include "list.h"
+
+List::List()
+{
+
+}
+
+
+List::~List()
+{
+
+}
+
+
+void List::Append(string newword)
+{
+
+    return;
+}
+
+
+void List::InsertAt(int pos, string newword)
+{
+
+    return;
+}
+
+
+void List::Delete(string someword)
+{
+
+    return;
+}
+
+
+void List::Replace(string oldword, string newword)
+{
+
+    return;
+}
+
+
+int List::Length() const
+{
+
+    return 0;
+}
+
+
+bool List::Find(string someword) const
+{
+
+    return true;
+}
+
+
